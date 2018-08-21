25ffca8a94bc88a081f4644909db894c72eb7645
Joao Lopes <joao.lopes@simplesurance.de>
2018-08-21 08:04:38 +0200

    wip products page

--- src/app.js
+++ src/app.js
@@ -1,16 +1,35 @@
-require('./style.scss')
+require('./css/style.scss')
 
 console.log('Hello. I see you love digging too. Our vegetables are grown with love and care. Thank you for checking our website. :)')
 
+function _(query) {
+  return document.querySelector(query)
+}
+
+function on(selector, event, callack) {
+  _(selector).addEventListener(event, callack)
+}
+
 window.addEventListener('scroll', function(e) {
-  var vh = Math.max(
+  const vh = Math.max(
     document.documentElement.clientHeight,
     window.innerHeight || 0)
 
-  if (window.scrollY > (0.5 * vh - 100)) {
-    document.querySelector('.header').classList.add('sticky')
-  } else {
-    document.querySelector('.header').classList.remove('sticky')
-  }
+  const logoHeight = _('.logo').clientHeight
 
+  if (window.scrollY > (0.5 * vh - logoHeight)) {
+    _('.header').classList.add('sticky')
+  } else {
+    _('.header').classList.remove('sticky')
+  }
 });
+
+on('.menu-button', 'click', () => {
+  document.body.classList.add('menu-open')
+})
+on('.overlay', 'click', () => {
+  document.body.classList.remove('menu-open')
+})
+on('.close-button', 'click', () => {
+  document.body.classList.remove('menu-open')
+})
